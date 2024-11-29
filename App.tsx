a9740f42b69386c40d799fc8d115e2e4caa15c7a
Vinay Kumar <47213644+0xVnay@users.noreply.github.com>
2024-11-29 21:11:50 +0530

    feat: add custom bottom sheet component with context

--- App.tsx
+++ App.tsx
@@ -2,30 +2,35 @@ import React from "react";
 import {
   SafeAreaView,
   StyleSheet,
-  View,
   KeyboardAvoidingView,
   Platform,
 } from "react-native";
 import { MessageList } from "./src/components/MessageList";
 import { ChatInput } from "./src/components/ChatInput";
 import { StatusBar } from "expo-status-bar";
+import { BottomSheetProvider } from "./src/context/BottomSheet";
 
 export default function App() {
   return (
-    <SafeAreaView style={styles.container}>
-      <KeyboardAvoidingView
-        style={styles.content}
-        behavior={Platform.OS === "ios" ? "padding" : undefined}
-      >
-        <MessageList />
-        <ChatInput />
-      </KeyboardAvoidingView>
-      <StatusBar />
-    </SafeAreaView>
+    <BottomSheetProvider>
+      <SafeAreaView style={styles.container}>
+        <KeyboardAvoidingView
+          style={styles.content}
+          behavior={Platform.OS === "ios" ? "padding" : undefined}
+        >
+          <MessageList />
+          <ChatInput />
+        </KeyboardAvoidingView>
+        <StatusBar />
+      </SafeAreaView>
+    </BottomSheetProvider>
   );
 }
 
 const styles = StyleSheet.create({
+  root: {
+    flex: 1,
+  },
   container: {
     flex: 1,
     backgroundColor: "#fff",
---
+++ src/context/BottomSheet.tsx
@@ -0,0 +1,109 @@
+import React, {
+  createContext,
+  useCallback,
+  useMemo,
+  useRef,
+  useState,
+} from "react";
+import {
+  BottomSheetBackdrop,
+  BottomSheetModal,
+  BottomSheetModalProvider,
+  BottomSheetView,
+} from "@gorhom/bottom-sheet";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+
+interface BottomSheetProviderProps {
+  children: React.ReactNode;
+}
+
+interface BottomSheetContextProps {
+  bottomSheetModalRef: React.RefObject<BottomSheetModal> | null;
+  showBottomSheet: () => void;
+  hideBottomSheet: () => void;
+  setContent: (content: React.ReactNode) => void;
+  setSnapIndex: (index: number) => void;
+  onDismissHandler: (callback: () => void) => void;
+}
+
+export const BottomSheetContext = createContext<BottomSheetContextProps>({
+  bottomSheetModalRef: null,
+  showBottomSheet: () => {},
+  hideBottomSheet: () => {},
+  setContent: () => {},
+  setSnapIndex: () => {},
+  onDismissHandler: () => {},
+});
+
+export const BottomSheetProvider = ({ children }: BottomSheetProviderProps) => {
+  const bottomSheetModalRef = useRef<BottomSheetModal>(null);
+  const [snapIndex, setSnapIndex] = useState<number>(0);
+  const snapPoints = useMemo(() => ["40%", "60%"], []);
+  const [dismissCallback, setDismissCallback] = useState<() => void>(() => {});
+  const [Content, setContent] = useState<React.ReactNode | null>(null);
+
+  const handlePresentModalPress = useCallback(() => {
+    bottomSheetModalRef.current?.present();
+    bottomSheetModalRef.current?.snapToIndex(snapIndex);
+  }, [snapIndex]);
+
+  const handleDismissModalPress = useCallback(() => {
+    bottomSheetModalRef.current?.dismiss();
+  }, []);
+
+  const handleSetContent = useCallback((content: React.ReactNode) => {
+    setContent(content);
+  }, []);
+
+  const handleSetSnapIndex = useCallback((index: number) => {
+    setSnapIndex(index);
+  }, []);
+
+  const handleOnDismiss = useCallback(() => {
+    if (dismissCallback) {
+      dismissCallback();
+    }
+  }, [dismissCallback]);
+
+  const handleOnDismissHandler = useCallback((callback: () => void) => {
+    setDismissCallback(() => callback);
+  }, []);
+
+  return (
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <BottomSheetModalProvider>
+        <BottomSheetContext.Provider
+          value={{
+            bottomSheetModalRef,
+            showBottomSheet: handlePresentModalPress,
+            hideBottomSheet: handleDismissModalPress,
+            setContent: handleSetContent,
+            setSnapIndex: handleSetSnapIndex,
+            onDismissHandler: handleOnDismissHandler,
+          }}
+        >
+          <>
+            {children}
+            <BottomSheetModal
+              index={snapIndex}
+              ref={bottomSheetModalRef}
+              keyboardBehavior="interactive"
+              keyboardBlurBehavior="restore"
+              enableContentPanningGesture={false}
+              onDismiss={handleOnDismiss}
+              backdropComponent={(backdropProps) => (
+                <BottomSheetBackdrop
+                  {...backdropProps}
+                  enableTouchThrough={true}
+                />
+              )}
+              snapPoints={snapPoints}
+            >
+              <BottomSheetView style={{ flex: 1 }}>{Content}</BottomSheetView>
+            </BottomSheetModal>
+          </>
+        </BottomSheetContext.Provider>
+      </BottomSheetModalProvider>
+    </GestureHandlerRootView>
+  );
+};
